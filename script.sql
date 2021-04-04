03ec2d1fde487e4f356c76c9f9b79997ba8b0de5
oalvarezr <oalvarezr@unal.edu.co>
2021-04-04 18:22:08 -0500

    add script.sql initial

---
+++ script.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE viwit;
+
+USE viwit;
+
+CREATE TABLE  credit_card(
+    credit_card_id INT(12) PRIMARY KEY,
+    credit_card_owner VARCHAR (50) NOT NULL,
+    ccv INT(3) NOT NULL,
+    expiration_date DATETIME NOT NULL
+);
+
+CREATE TABLE token(
+    token_id INT (12) AUTO_INCREMENT PRIMARY KEY,
+    token VARCHAR (128) NOT NULL,
+    expiration_date TIMESTAMP NOT NULL,
+    creation_date TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE user_viwit (
+    user_id INT(12) PRIMARY KEY,
+    firstname VARCHAR (30) NOT NULL,
+    lastname VARCHAR (30) NOT NULL,
+    email VARCHAR (50) NOT NULL,
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_pasword VARCHAR (128) NOT NULL,
+    credit_card INT(12),
+    token_id INT (12),
+    block_account BOOL NOT NULL,
+    FOREIGN KEY (credit_card) REFERENCES credit_card(credit_card_id),
+    FOREIGN KEY (token_id) REFERENCES token(token_id)
+);
+
+CREATE TABLE login(
+    login_id INT (32) AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(12) NOT NULL,
+    login_date TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_viwit (user_id)
+);
+
